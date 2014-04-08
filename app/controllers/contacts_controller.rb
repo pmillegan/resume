90b006867417771cb19bc8bfff9a74f92b9c06be
Ryan Booth <ryanbooth77@gmail.com>
2014-04-07 19:41:34 -0700

    Not really fixed. The SMTP settings are the issue. You need a server to route through.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
