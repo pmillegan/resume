2b3667d0e441d575315a0b0e6a2638de9f7c730e
Patrick <patrickmillegan@gmail.com>
2014-04-30 23:42:43 -0700

    added dynamic page loading

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,9 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
 $('#myTab a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
