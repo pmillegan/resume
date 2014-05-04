a544398fbdaf2cbf152958f59e7c1825010a656b
Patrick <patrickmillegan@gmail.com>
2014-05-03 23:35:32 -0700

    polishing up email form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     if @contact.deliver
       @success = 'Thank you for your message. I will contact you soon!'
     else
-      @error = "Could not send message."
+      @error = "Could not send message. Make sure all fields are filled out :)"
     end
   end
 end
